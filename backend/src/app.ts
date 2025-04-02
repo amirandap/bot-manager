8a1adab95d96b132d45a0a92b4a6be954b2707e0
Uriel Campos <urielcampos448@gmail.com>
2025-04-02 14:56:53 -0300

    feat: add dotenv and morgan for environment variable management and logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import { setBotsRoutes } from './routes/botsRoutes';
 import { setStatusRoutes } from './routes/statusRoutes';
 import cors from 'cors';
+import morgan from 'morgan';
+import { WAPI_URL } from './constants/Urls';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('short'))
 
 setBotsRoutes(app);
 setStatusRoutes(app);
